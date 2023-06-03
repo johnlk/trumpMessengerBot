426a0d626fa17c47191e93b6b3aaec7ff932e2d2
johnlk <john@useintegral.com>
2023-06-03 10:12:59 -0400

    Add package.json, readme cleanup, index.js cleanup

--- index.js
+++ index.js
@@ -1,160 +1,97 @@
-//This is still work in progress
-/*
-Please report any bugs to nicomwaks@gmail.com
+// This is a work in progress.
+// Please report any bugs to nicomwaks@gmail.com.
 
-i have added console.log on line 48 
+'use strict';
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 
+const app = express();
+const port = process.env.PORT || 8080;
+const token = "EAAcF2t4BOS4BAA2umCekrT7DqDTglFuRrDwEqWjCpXRxZALJKydN2X5267EqvP3ZBKZCGXQ90fU0GpDxBig9yAIXkdUPJHLDSR6ZCwCRuZBg94i4b3WjrtFn8pFUHHHUmWzu3ZCiWatZA9Mlr1ZBDWN8vnAEl40sYFHhdPb0bAxRaAGBGQeAvDYu";
 
+let trumpData;
 
- */
-'use strict'
+const fs = require('fs');
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-
-var trumpData;
-
-
-var fs = require('fs');
- 
 fs.readFile('./output/all.txt', 'utf8', function(err, contents) {
-	trumpData = contents.split('\n');
-    // console.log(data[0]);
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  trumpData = contents.split('\n');
 });
 
-app.set('port', (process.env.PORT || 8080))
+app.set('port', port);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// index
 app.get('/', function (req, res) {
-	res.send('hello world')
-})
+  res.send('Hello World');
+});
 
-// for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'greatscott') {
-		res.send(req.query['hub.challenge'])
-	} else {
-		res.send('Error, wrong token')
-	}
-})
+  if (req.query['hub.verify_token'] === 'greatscott') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong token');
+  }
+});
 
-// to post data
 app.post('/webhook/', function (req, res) {
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = req.body.entry[0].messaging[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-			
-			var strRes = trumpData[Math.floor(Math.random()*trumpData.length)];
-			while(strRes.length < 5){
-				strRes = trumpData[Math.floor(Math.random()*trumpData.length)];
-			}
-			// console.log(strRes);
-
-			sendTextMessage(sender, strRes)
-		}
-		if (event.postback) {
-			let text = JSON.stringify(event.postback)
-
-			var strRes = trumpData[Math.floor(Math.random()*trumpData.length)];
-			while(strRes.length < 5){
-				strRes = trumpData[Math.floor(Math.random()*trumpData.length)];
-			}
-			// console.log(strRes);
-
-			sendTextMessage(sender, strRes, token)
-			continue
-		}
-	}
-	res.sendStatus(200)
-})
-
-
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = "EAAcF2t4BOS4BAA2umCekrT7DqDTglFuRrDwEqWjCpXRxZALJKydN2X5267EqvP3ZBKZCGXQ90fU0GpDxBig9yAIXkdUPJHLDSR6ZCwCRuZBg94i4b3WjrtFn8pFUHHHUmWzu3ZCiWatZA9Mlr1ZBDWN8vnAEl40sYFHhdPb0bAxRaAGBGQeAvDYu"
+  const messagingEvents = req.body.entry[0].messaging;
+  for (let i = 0; i < messagingEvents.length; i++) {
+    const event = messagingEvents[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      const text = event.message.text;
+      let strRes = getRandomResponse();
+      while (strRes.length < 5) {
+        strRes = getRandomResponse();
+      }
+      sendTextMessage(sender, strRes);
+    }
+    if (event.postback) {
+      const text = JSON.stringify(event.postback);
+      let strRes = getRandomResponse();
+      while (strRes.length < 5) {
+        strRes = getRandomResponse();
+      }
+      sendTextMessage(sender, strRes);
+      continue;
+    }
+  }
+  res.sendStatus(200);
+});
 
 function sendTextMessage(sender, text) {
-	let messageData = { text:text }
-	
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
+  const messageData = { text };
+  
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: token },
+    method: 'POST',
+    json: {
+      recipient: { id: sender },
+      message: messageData,
+    },
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages:', error);
+    } else if (response.body.error) {
+      console.log('Error:', response.body.error);
+    }
+  });
 }
 
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
+function getRandomResponse() {
+  if (!trumpData || trumpData.length === 0) {
+    return '';
+  }
+  return trumpData[Math.floor(Math.random() * trumpData.length)];
 }
 
-// spin spin sugar
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+app.listen(port, function() {
+  console.log('Running on port', port);
+});
